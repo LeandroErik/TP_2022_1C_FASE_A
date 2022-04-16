ff0a457b677770c9ead11bd71b128ccf525b1b06
sebipapsu <sebipaps@gmail.com>
2022-04-15 21:20:11 -0300

    fix: cambio en el brief

--- shared/include/shared/hello.h
+++ shared/include/shared/hello.h
@@ -1,4 +1,3 @@
-
 #ifndef SHARED_HELLO_H_
 
 #define SHARED_HELLO_H_
@@ -6,9 +5,8 @@
     #include <stdlib.h>
     #include <stdio.h>
 
-    /**
-    * @brief: Imprsime un saludo al nombre que se pase por parámetro por consola.
-    * @param who: A quien se saluda.
+    /** @brief Imprsime un saludo al nombre que se pase por parámetro por consola.
+    * @param who A quien se saluda.
     * @return los caracteres.
     */
     int say_hello(char* who);
