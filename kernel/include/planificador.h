db9d9a820b19989d53c3aa361270a3da870d97ce
LeandroErik <erikquispechoque@gmail.com>
2022-04-21 23:16:30 -0300

    agrego funcion carga configuracion

---
+++ kernel/include/planificador.h
@@ -0,0 +1,20 @@
+#ifndef KERNEL_UTILS_H_
+#define KERNEL_UTILS_H_
+
+#include <commons/log.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdint.h>
+
+typedef struct
+{
+	uint32_t pid;
+	uint32_t tamanio;
+	t_list lista_instrucciones;
+	uint32_t proxima_instruccion;
+	uint32_t tabla_de_paginas;
+	float estimacion_rafaga;
+} pcb;
+typedef
+
+#endif
