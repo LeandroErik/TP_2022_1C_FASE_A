e294a21f25f9700777b8d7b651f154a4b01d15a1
FelipeAlborch <82057459+FelipeAlborch@users.noreply.github.com>
2022-04-23 20:30:51 -0300

    Creamos y obtenemos valores de cpu.config

--- cpu/src/main.c
+++ cpu/src/main.c
@@ -1,9 +1,20 @@
 #include <cpu_utils.h>
+#include <commons/config.h>
 
 void conectar_memoria(void);
 
 int main(int argc, char *argv[])
 {
+  t_config* config = config_create("CPU.config");
+
+  int entradas_tlb = config_get_int_value(config, "ENTRADAS_TLB");
+  char* reemplazo_tlb = config_get_string_value(config, "REEMPLAZO_TLB");
+  int retardo_noOp = config_get_int_value(config, "RETARDO_NOOP");
+  char* ip_memoria = config_get_string_value(config, "IP_MEMORIA");
+  int puerto_memoria = config_get_int_value(config, "PUERTO_MEMORIA");
+  int puerto_escucha_dispatch = config_get_int_value(config, "PUERTO_ESCUCHA_DISPATCH");
+  int puerto_escucha_interrupt = config_get_int_value(config, "PUERTO_ESCUCHA_INTERRUPT");
+
   logger = log_create("CPU.log", "CPU", true, LOG_LEVEL_DEBUG);
 
   int socketCPU = iniciar_servidor_cpu(logger);
@@ -41,4 +52,4 @@ void conectar_memoria(void)
     enviar_mensaje("soy CPU, pa", socketCpuCliente);
 
     liberar_conexion_con_memoria(socketCpuCliente);
-}
+} 
