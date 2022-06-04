b105370bab68d49f9a14eab73e0343f5ec72a30f
Sebastián Papanicolau <37161571+sebipap@users.noreply.github.com>
2022-06-04 16:40:45 -0300

    .

--- CPU/src/cpu_config.c
+++ CPU/src/cpu_config.c
@@ -6,6 +6,8 @@ void rellenar_configuracion_cpu(Config *config)
   CPU_CONFIG.REEMPLAZO_TLB = config_get_string_value(config, "REEMPLAZO_TLB");
   CPU_CONFIG.RETARDO_NOOP = config_get_int_value(config, "RETARDO_NOOP");
   CPU_CONFIG.IP = config_get_string_value(config, "IP");
+    CPU_CONFIG.IP_MEMORIA = config_get_string_value(config, "IP_MEMORIA");
+
   CPU_CONFIG.PUERTO_MEMORIA = config_get_string_value(config, "PUERTO_MEMORIA");
   CPU_CONFIG.PUERTO_KERNEL_DISPATCH = config_get_string_value(config, "PUERTO_KERNEL_DISPATCH");
   CPU_CONFIG.PUERTO_KERNEL_INTERRUPT = config_get_string_value(config, "PUERTO_KERNEL_INTERRUPT");
