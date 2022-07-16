297655f633e042c67fc60ccf558148f80e0c5491
AgusB1101 <agustinbernal69@gmail.com>
2022-07-16 16:54:11 -0300

    CPU: Cambiar settings.mk para muestreo de memcheck

--- CPU/settings.mk
+++ CPU/settings.mk
@@ -13,5 +13,5 @@ CRELEASE=-O3 -Wall -DNDEBUG
 ARGS=
 
 # Valgrind flags
-MEMCHECK_FLAGS=--track-origins=yes --log-file="memcheck.log"
+MEMCHECK_FLAGS=--leak-check=full --show-leak-kinds=all --track-origins=yes --log-file="memcheck.log"
 HELGRIND_FLAGS=--log-file="helgrind.log"
